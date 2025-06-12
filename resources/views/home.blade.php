df0d5a83052fa06f27c77aa0ec4e53b55f6688b9
DrKetamine <131555418+DrKetamine@users.noreply.github.com>
2025-06-12 19:45:32 +0200

    Edited the homepae, testing commit as well

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <title>My Project Home</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <h1>Welcome to OSRSXpier</h1>
+    <p>This is the homepage.</p>
+</body>
+</html>
